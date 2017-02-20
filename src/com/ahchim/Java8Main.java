3df050b15d092f467d4da2360a0d5e99d420741d
a.ahchim.lee <umie333@gmail.com>
2017-02-20 15:20:17 +0900

    Java8 LocalDate 클래스 사용 예제

--- src/com/ahchim/Java8Main.java
+++ src/com/ahchim/Java8Main.java
@@ -1,19 +1,42 @@
 package com.ahchim;
 
+import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.TemporalAdjusters;
 
 import com.ahchim.util.TimeUtil;
 
 public class Java8Main {
 	public static void main(String args[]){
-		calcDurationBySystemMili();
-		calcDuration();
-		calcDurationBySystemMili();
-		calcDuration();
-		calcDurationBySystemMili();
-		calcDuration();
+		//calcDurationBySystemMili();
+		//calcDuration();
+		
+		getDate();
+		adjustDate();
 	}
+	
+	// today.with() 함수 사용하기 : TemporalAdjusters.dayOfWeekInMonth 함수 사용
+	public static void adjustDate(){
+		LocalDate today = LocalDate.now();
+		
+		LocalDate next = today.with(TemporalAdjusters.dayOfWeekInMonth(2, DayOfWeek.SUNDAY));
+		
+		System.out.println("2월 2번째 일요일: " + next);
+	}
+	
+	// LocalDate로 날짜 받아오기
+	public static void getDate(){
+		LocalDate today = LocalDate.now();
+		System.out.println("today: " + today);
+		LocalDate myBirthday = LocalDate.of(1987, 6, 25);
+		System.out.println("My Birthday: " + myBirthday);
+		myBirthday = LocalDate.of(1987, Month.APRIL, 25);
+		System.out.println("My Birthday: " + myBirthday);
+	}
+	
 	
 	// System.currentTimeMillis()를 이용한 소요시간 계산
 	public static void calcDurationBySystemMili(){
